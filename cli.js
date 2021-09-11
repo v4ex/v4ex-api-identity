c0ee492a47187f2989e8bd5f0d2f887afc2b37df
Xuwei Li <dev@xuwei.li>
2021-09-11 11:36:23 +0000

    Add shebang to cli.js.

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
+#!/usr/bin/env node
 /* Copyright (c) V4EX Inc. SPDX-License-Identifier: GPL-3.0-or-later */
 
 // Purpose: Command Line Interface entry.
-
 
 
 const { program } = require('commander')
